b2c2d54a44b3a33b06b81bae415f9d75d163b434
Hihhh-lilyJ <eggylilwin@gmail.com>
2023-03-05 16:15:36 +0630

    Initial Commit

---
+++ SwiftUIMVVM/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  SwiftUIMVVM
+//
+//  Created by MacBook Pro on 05/03/2023.
+//
+
+import SwiftUI
+
+struct ToDoListItem: Identifiable {
+    var id = UUID()
+    var action: String
+}
+
+class ToDoList: ObservableObject {
+    @Published var items: [ToDoListItem] = []
+}
+
+struct ContentView: View {
+    @ObservedObject var viewModel: ToDoList = ToDoList()
+    var body: some View {
+        NavigationView {
+            VStack {
+                List(viewModel.items) {
+                    item in
+                    Text(item.action)
+                }
+            }
+            .navigationTitle("To Do List")
+            .navigationBarItems(trailing: Button(action: {
+                
+                self.viewModel.items = [
+                    ToDoListItem(action: "Go Running"),
+                    ToDoListItem(action: "Get Milk"),
+                    ToDoListItem(action: "Do Work"),
+                    ToDoListItem(action: "Upload Video"),
+                    ToDoListItem(action: "Teach SwiftUI")
+                ]
+                
+            }, label: {
+                Text("Refresh")
+            }))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIMVVM/SwiftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMApp.swift
+//  SwiftUIMVVM
+//
+//  Created by MacBook Pro on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
